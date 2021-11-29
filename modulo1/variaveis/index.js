14d9faeafe8e521857469d980a7f73d88f2b3443
Ewerton Francis <ewerton.lost2@gmail.com>
2021-11-29 16:54:54 -0300

    index.html

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,50 @@
+// permite escrer comentarios
+
+// comenta uma linha
+
+/*este é um texto grande 
+ que vai virar um bloco*/
+
+//console.log("Olá Mundo")
+
+//const nomeDoUsuario = prompt("Qual é o seu nome?")
+//const idadeDoUsuario = prompt("Qual a sua idade?")
+
+// Ewercicio 1
+/*const nome = ""
+const idade = ""
+console.log = (",typeof")
+
+// apareceu o nome live reoled enable acredito que seja pelo fato de não haver uma declaração */
+
+const nome = "Ewerton"
+const idade = 31
+console.log("nome", typeof nome)
+console.log("idade" ,typeof idade)
+
+// o fato de eu ter atribuido um valor a variavel o teype off me mostra o tipo dele
+
+/*const nome = "Ewerton"
+const idade = 31
+console.log("olá", nome, "você tem", idade, "anos")*/
+
+// quest 3 Duvida não consegui resolver
+
+let corDaRoupa = prompt("Você esta usando azul hoje")
+let corDaCamisa= prompt("Você está usando azul hoje")
+let corDoJeans = prompt("você está de azul hoje")
+console.log(corDaRoupa=true,corDaCamisa=false,corDoJeans=false)
+
+// quest 4
+
+let a = 10
+let b = 25
+a = b
+c = 10
+b = c
+
+
+console.log("novo valor de a é",25)
+console.log("novo valor de b é",10)
+
+console.log(a,b) 
