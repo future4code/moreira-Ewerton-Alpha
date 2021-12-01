a946dc1e168bced4cee259290e4efa734a7765fb
Ewerton Francis <ewerton.lost2@gmail.com>
2021-12-01 17:00:32 -0300

    index.js

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,30 @@
+// Exericio de interpretação
+/*a.  undefined
+731-b07794b55fe2713d09a5.js:1 b.  null
+731-b07794b55fe2713d09a5.js:1 c.  11
+731-b07794b55fe2713d09a5.js:1 d.  3
+731-b07794b55fe2713d09a5.js:1 e.  (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+731-b07794b55fe2713d09a5.js:1 f.  9
+//1) exercicio de código*/
+
+/*const emailDoUsuario = prompt("email do usuário")
+  const nomeDoUsuario =  prompt("Qual é o seu nome")
+  const frase=`O email ${emailDoUsuario} foi cadastrado com sucesso!Seja bem-vindo(o),${nomeDoUsuario}.`
+  console.log(frase)*/
+
+//2)Exercicio 2
+
+const comidasPreferidas = ["pizza","açaí","hamburguer","sushi"]
+//console.log(comidaspreferidas)
+//const frase=`Essas são as minhas comidas preferidas ${comidaspreferidas}`
+//console.log(frase)
+const comidaFavoritaUsuario = prompt("Qual a sua comida Favorita ?")
+const segundoItem=comidasPreferidas[1]
+console.log(comidasPreferidas)
+
+/* exercicio 3 
+let listadetarefas =[]
+let tarefasusuario= prompt("escreva 3 tarefas que você precise realizar hoje")
+listadetarefas.push(tarefasusuario)
+console.log(listasdetarefas)*/
+
