34de5c7b85732f29d4f946816c41e9c0786e301d
Ewerton Francis <ewerton.lost2@gmail.com>
2021-12-07 19:23:58 -0300

    exercicio de condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,84 @@
+// exercicio de interpretação
+//1)
+//a)eleverifica se o numero escolhido pelo 
+//usuario é divisivel por 2 sem resto
+//b)numeros divisiveis por 2 
+//c)numeros não divisiveis por 2 
+
+//2)
+//a)dar  o valor au usuario de acordo com a 
+// a escolha da fruta .
+//b)a resposta vai ser r$ 2,25 
+//c)O preço da fruta seria 5 reais 
+
+//3)
+//a) perguntando ao usuario para digitar um numero
+//b) 10 , diz que o numero passou no teste 
+//  -10 ,diz que é indefino 
+// c) existe erro no console . A falta da condicional 
+//Else para alternar o valor de falso.(ACHO QUE É ISSO)
+
+// Exerciicio de Escrita
+
+//1) 
+//a)
+/*
+const perguntaAoUsuario= Number(prompt("qual a sua idade"))
+if(perguntaAoUsuario>18){
+    console.log("Você pode dirigir.")
+} else{
+    (perguntaAoUsuario<18)
+    console.log("Você não pode dirigir")
+}
+*/ 
+//2)
+/*
+let perguntaDoUsuario=(prompt(`qual periodo do dia você estuda?, responda M para matutino,V vespertino, N noturno`))
+
+respostaDoUsuario=`M`
+respostaDoUsuario=`V`
+respostaDoUsuario=`N`
+
+ if(respostaDoUsuario === `M`){
+    console.log("Bom Dia")
+}else if (respostaDoUsuario === `V`){
+    console.log("Boa Tarde")
+}else {
+    (respostaDoUsuario===`N`)
+    console.log("Boa Noite")
+}
+/*
+//3)como colocar twolloer case ?
+// se fossem 2 perguntas como na quat?
+let turnoDeEstudo=(prompt(`escolha entre M para matutino,V para vespertino,N para  notutno.`))
+switch(turnoDeEstudo) {
+    case "M":
+        console.log("Bom Dia")
+        break
+    case "V":
+        console.log("Boa Tarde")
+        break
+    case "N":
+        console.log("Boa Noite")
+        break
+        defaut:
+        console.log(`escolha entre M para matutino,V para vespertino,N para  notutno.`)
+}
+*/
+//4)/*
+let perguntaAoUsuario1=(prompt(`Qual o gênero do filme que vamos assistir?`))
+let perguntaAoUsuario2=Number(prompt(`Quanto será o preço do ingresso?`))
+    
+if (perguntaAoUsuario1 =`Fantasia` && perguntaAoUsuario2 < 15){
+    console.log(`Bom filme!`)
+}else{
+    console.log(`Assistir outro filme`)
+
+}
+    
+
+
+
+
+
+
