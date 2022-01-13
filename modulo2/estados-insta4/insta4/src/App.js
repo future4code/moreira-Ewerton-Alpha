29644e7e60840bb50c51ae9532159c34fb956104
unknown <ewerton.lost2@gmail.com>
2022-01-13 20:31:28 -0300

    atualização-insta4

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -11,29 +11,45 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-         />
-        
-        <Post
-          nomeUsuario={'luis'}
-          fotoUsuario={'https://images.unsplash.com/photo-1610023787041-37c6f68fd375?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'}
-          fotoPost={'https://images.unsplash.com/photo-1621609764095-b32bbe35cf3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'}
-        />
+//ficar atento a sintaxe do objetos no array e a sintaxe do texto
+  state={
+        listaDosPost: [ {
+          
+          nomeUsuario:'Laura',
+          fotoUsuario:'https://picsum.photos/50/50',
+          fotoPost:'https://picsum.photos/200/150',
+         
+        },
+        { nomeUsuario:'Lucas',
+          fotoUsuario:'https://images.unsplash.com/photo-1610023787041-37c6f68fd375?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+          fotoPost:'https://images.unsplash.com/photo-1621609764095-b32bbe35cf3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
 
-        <Post
-          nomeUsuario={'zebra'}
-          fotoUsuario={'https://images.unsplash.com/photo-1641982343852-3d75c2eb7e3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'}
-          fotoPost={'https://images.unsplash.com/photo-1641990458432-9c60bcdf8e84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'}
-          />
+        },
+        { nomeUsuario:'Luna',
+          fotoUsuario:'https://images.unsplash.com/photo-1641982343852-3d75c2eb7e3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+          fotoPost:'https://images.unsplash.com/photo-1641990458432-9c60bcdf8e84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+
+        }]
+      }
+
+      render(){
+       
+       
+        const listaDosPost=this.state.listaDosPost.map((post,index)=>{
+        return <Post key={index}
+        nomeUsuario={post.nomeUsuario}
+        fotoUsuario={post.fotoUsuario}
+        fotoPost={post.fotoPost}
+        
+         />
+        })
+      
+       return (
+      <MainContainer>
+        {listaDosPost}
       </MainContainer>
-    );
-  }
+      );
+    }
 }
 
 export default App;
