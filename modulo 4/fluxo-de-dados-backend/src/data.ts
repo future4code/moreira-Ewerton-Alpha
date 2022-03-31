2b4eff5df9d5524eaecdd4bd966375b51a49dce3
unknown <ewerton.lost2@gmail.com>
2022-03-30 21:52:51 -0300

    fluxo-de-dados-backend

---
+++ modulo 4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,24 @@
+type produtos = {
+    id: string;
+    name: string;
+    price: number
+ } 
+ 
+ 
+ export const produtos: produtos[] = [ 
+    {
+        id: "1",
+        name: "mac",
+        price: 40000
+    },
+    {
+        id: "2",
+        name: "windows",
+        price: 30000
+    },
+    {
+        id: "3",
+        name: "linux",
+        price: 2000
+    },
+ ]
