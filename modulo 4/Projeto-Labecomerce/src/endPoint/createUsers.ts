ed4c973066d4c854546ed6b6e5939b36335dc068
unknown <ewerton.lost2@gmail.com>
2022-04-19 07:58:00 -0300

    atualizacao1

---
+++ modulo 4/Projeto-Labecomerce/src/endPoint/createUsers.ts
@@ -0,0 +1,21 @@
+import express, { Express ,Request , Response} from "express";
+import connection from "../connection"
+
+
+export default async function createUsers (
+    req:Request,
+    res:Response
+):Promise<void>{
+
+    try{
+        const { username , email , pwd } = req.body
+        
+        await connection ("labecommerce_users")
+        .insert({username,email ,pwd})
+         
+        res.status(201).end()
+    } catch (error){
+        res.status(500).end()
+    }
+    
+}
