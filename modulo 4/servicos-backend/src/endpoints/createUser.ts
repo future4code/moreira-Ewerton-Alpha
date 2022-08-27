d29767e71cb751e09421473e4ba3ea68afd76420
unknown <ewerton.lost2@gmail.com>
2022-08-27 13:00:50 -0300

    revisao

--- modulo 4/servicos-backend/src/endpoints/createUser.ts
+++ modulo 4/servicos-backend/src/endpoints/createUser.ts
@@ -1,14 +1,41 @@
 import {Request,Response} from "express"
 import { getAddressInfo } from "../services/getAddressinfo"
+import transporter from "../services/mailtranspoter"
 
 
 
 export const createUser = async (req:Request,res:Response)=>{
     try {
-        const {zipcode} = req.body
-        const address = await(getAddressInfo(zipcode))
-        res.send("teste executado")
-    } catch (error) {
-        res.send({message: "error inesperado"})
+       // const {zipcode} = req.body  ou 
+       const {zipcode,email,passaword} = req.body
+
+        const address = await getAddressInfo(zipcode)
+        if(!address) {
+          throw new Error (" Error on get address")
+        }
+
+        //quando pegar cep quero enviar o email ex:
+// corpo do email normalmente vai ser um html
+
+const emailInfo = await transporter.sendMail({
+    from: `${process.env.NODEMAILER_USER}`,
+    to: `${email}`,
+    subject: "ususario criado com sucesso",
+    text: "Seu usuario foi criado com sucesso",
+    html: `<p><strong>Ola parabends por se inscrever conosco.</strong>Segue seus dados:
+    password: <em>${passaword}</em>
+    endereço :${address.city}/${address.state} ${address.district} ${address.street}</p>`
+   
+})
+
+        res.send({emailInfo , message:"Cadastro concluido com sucesso"})
+    } catch (error:any) {
+        if(error instanceof Error){
+            res.send({error,message:error.message})
+        } else {
+            res.send({error, message:error.message})
+        }
+        
+        
     }
 }
--- modulo 4/servicos-backend/src/services/getAddressinfo.ts
+++ modulo 4/servicos-backend/src/services/getAddressinfo.ts
@@ -1,15 +1,25 @@
 import axios from "axios";
+import { Address } from "../types/Adress";
 
 //https://viacep.com.br/ws/:cep/json/  
 
 const baseUrl = "https://viacep.com.br/ws/"
 
-export const getAddressInfo = async (zipcode:string)=>{
+export const getAddressInfo = async (zipcode:string) : Promise <Address | null >=>{
 
     try {
-        const response = await axios.get('${baseUrl}/${zipcode}/json/')
-        console.log(response)
+        const response = await axios.get(`${baseUrl}/${zipcode}/json/`)
+        //console.log(response)
+        const address: Address = {
+            state: response.data.uf,
+            city: response.data.localidade,
+            district:response.data.district,
+            street: response.data.logradouro
+        }
+
+        return address
     } catch (error) {
+        //importante deixar claro esse error
         console.error("Erro no serviço getAddressInfo:",error)
         return null 
     }
